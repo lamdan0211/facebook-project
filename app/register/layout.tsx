7718ab9fca796b6748eaa750f886d931273b18dc
lamdan0211 <57250382+lamdan0211@users.noreply.github.com>
2025-06-09 18:32:49 +0700

    update

--- app/register/layout.tsx
+++ app/register/layout.tsx
@@ -2,6 +2,8 @@ import '../globals.css';
 
 export default function RegisterLayout({ children }: { children: React.ReactNode }) {
   return (
-    {children}
+    <div className="flex flex-col min-h-screen">
+      {children}
+    </div>
   );
 } 
